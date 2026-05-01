64635b08287f47a913f2b83238ac71d3146d2d76
Ezermod <ppolaralves@gmail.com>
2026-05-01 14:24:12 -0300

    Add flash Gapps in roms vannila and permission for system.prop
    Add TARGET_SYSTEM_PROP to device.mk and adjust includes.

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ TARGET_IS_VAB := true
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+-include vendor/lineage/config/BoardConfigReservedSize.mk
+
 # Screen
 TARGET_SCREEN_DENSITY := 400
 
