88d4ddc70f69bcfe06cb147e4406a4f7c42a6802
Ezermod <ppolaralves@gmail.com>
2026-05-14 23:24:17 -0300

    Update device configuration for lineage_xpeng

--- lineage_xpeng.mk
+++ lineage_xpeng.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/xpeng/device.mk)
 
 # Inherit some common Alpha stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := infinity_xpeng
+PRODUCT_NAME := lineage_xpeng
 PRODUCT_DEVICE := xpeng
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
@@ -27,11 +27,20 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=motorola/xpeng_g/xpeng:12/S1RXS32.50-13-25/5fb68-c44485:user/release-keys \
     DeviceProduct=xpeng_retail
 
-#infinity flags
-INFINITY_MAINTAINER := "EzerMod"
+#axionos flags
+TARGET_ENABLE_BLUR := true
 
-# Whether the device supports Fingerprint On Display
-TARGET_HAS_UDFPS := false
+TARGET_INCLUDE_AXFX := true
 
-# Whether Including Google Apps
-WITH_GAPPS := false #true
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 108,8,2
+AXION_CAMERA_FRONT_INFO := 16
+
+# Maintainer name (underscores become spaces in the UI)
+AXION_MAINTAINER := EzerMod   
+
+# Processor name (underscores become spaces)
+AXION_PROCESSOR := Snapdragon_888+_5G
+
+
+TARGET_SUPPORTED_REFRESH_RATES := 45,60,75,90,120,144
